57a3cd4ff783e079fd5acccecbbb1f0b0870ec7b
Dario Proaño <yashio200007@gmail.com>
2022-05-04 23:58:34 -0300

    Desafio n°4

---
+++ clase8/clase8.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+
+class Contenedor{
+    constructor(rutaDeArchivo){
+        this.rutaDeArchivo = rutaDeArchivo;
+    }
+
+    async save(obj){
+        const objs = await this.getAll()
+        console.log(objs)
+
+        let newId
+        if (objs.length == 0) {
+          newId = 1
+        } else {
+          newId = objs[objs.length - 1].id + 1
+        }
+    
+        const newObj = { ...obj, id: newId }
+        objs.push(newObj)
+    
+        try {
+          await fs.promises.writeFile(this.rutaDeArchivo, JSON.stringify(objs, null, 2))
+          return newId
+        } catch (error) {
+          throw new Error(`Error al guardar: ${error}`)
+        }
+    }
+
+    async getById(numeroId){
+        try{
+            let listaProductos = JSON.parse(await fs.promises.readFile(this.rutaDeArchivo, 'utf-8'))
+            let productoEncontrado = listaProductos.find((producto) => {
+                return producto.id === numeroId
+            })
+            if (productoEncontrado == (undefined || null)){
+                console.log('El producto no se encontró.');
+            }
+            else{
+                console.log('El producto encontrado es: ', productoEncontrado);
+                return productoEncontrado;
+            }
+
+        }
+        catch(err){
+            throw new Error (`Error al traer el producto: ${err}`)
+
+        }
+    }
+
+    async getAll(){
+        try{
+            let listaProductos = await fs.promises.readFile(this.rutaDeArchivo, 'utf-8')
+            console.log(listaProductos)
+            return JSON.parse(listaProductos)
+        }
+        catch(err){
+            return []
+        }
+    }
+
+    async deleteById(numeroId){
+        try{
+            let listaProductos = JSON.parse(await fs.promises.readFile(this.rutaDeArchivo, 'utf-8'))
+            let productoEliminado = listaProductos.filter((producto) => {
+                return producto.id !== numeroId
+            })
+            console.log(productoEliminado);
+            fs.promises.writeFile(this.rutaDeArchivo, JSON.stringify(productoEliminado, null, 2))
+        }
+        catch (err){
+            throw new Error(`Hubo un error al eliminar id del producto: ${err}`)
+        }
+    }
+
+    async deleteAll(){
+        try{
+            await fs.promises.writeFile(this.rutaDeArchivo, JSON.stringify([]), null, 2)
+            console.log('Se limpió la lista de productos')
+        }
+        catch(err){
+            throw new Error(`Hubo un error: ${err}`)
+        }
+    }
+
+    async getByIdRandom(){
+        try{
+            let listaProductos = JSON.parse(await fs.promises.readFile(this.rutaDeArchivo, 'utf-8'))
+            let productoRandom = listaProductos[Math.floor(Math.random() * listaProductos.length)]
+            return productoRandom;
+        }
+        catch (err){
+            throw new Error ('Hubo un error: ', err)
+        }
+
+    }
+}
+
+
+
+module.exports = Contenedor
